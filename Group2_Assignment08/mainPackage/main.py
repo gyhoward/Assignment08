78318989e9ef69b053f6229009e4354cf66ec1fb
joshu <joshu@DESKTOP-137O61M.wifi.uc.edu>
2024-03-28 12:14:37 -0400

    first commit

--- Group2_Assignment08/mainPackage/main.py
+++ Group2_Assignment08/mainPackage/main.py
@@ -1,11 +1,22 @@
 #main.py
 
+# Name:Gillian Howard
+# email:howardgy@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date: 3/28/2024
+# Course/Section: IS4010
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: make a query and print statement with output
+# Brief Description of what this module does: This module calls our print statement
+# Citations:
+# Anything else that's relevant:
+
 from statementPackage.statement import *
 from queryPackage.query import *
 
 
 if __name__ == "__main__":    
     factualStatement = print_statement()
-    query = query_of_data()
+    
     
     
